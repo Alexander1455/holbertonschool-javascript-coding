0bb654f4a85cd61cdb4f5b368f83f0d2ac46bfda
Alexander1455 <alexander21111121@gmail.com>
2023-10-23 00:11:43 -0500

    advanced

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,37 @@
 #!/usr/bin/node
-const axios = require('axios').default;
-const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+const request = require('request');
 
-axios.get(url)
-  .then(({ data }) => {
-    /* console.log(data.characters); */
-    data.characters.forEach((char) => {
-      axios.get(char)
-        .then(({ data }) => console.log(data.name))
-        .catch(err => console.log(err.message));
-    });
-  })
-  .catch(err => console.log(err.message));
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Usage: node 100-starwars_characters.js <Movie ID>');
+} else {
+  const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else if (response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+
+      // Obtener la lista de personajes
+      const characters = movieData.characters;
+
+      // Realizar solicitudes para obtener los nombres de los personajes
+      characters.forEach((characterUrl) => {
+        request.get(characterUrl, (charError, charResponse, charBody) => {
+          if (charError) {
+            console.error(charError);
+          } else if (charResponse.statusCode === 200) {
+            const characterData = JSON.parse(charBody);
+            console.log(characterData.name);
+          } else {
+            console.error(`Request failed with status code: ${charResponse.statusCode}`);
+          }
+        });
+      });
+    } else {
+      console.error(`Request failed with status code: ${response.statusCode}`);
+    }
+  });
+}
