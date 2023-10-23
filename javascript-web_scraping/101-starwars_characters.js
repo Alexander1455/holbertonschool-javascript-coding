fc73585df7b1af61f6c8a3c0f617e038cf5bc332
Alexander1455 <alexander21111121@gmail.com>
2023-10-23 00:14:12 -0500

    advanced

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,17 +1,43 @@
 #!/usr/bin/node
-const axios = require('axios');
+const request = require('request');
 
-const getChar = async (url) => {
-  const name = await axios.get(url);
-  return (name.data.name);
-};
+const movieId = process.argv[2];
 
-const getAll = async (url) => {
-  const response = await axios.get(url);
-  for (const character of response.data.characters) {
-    const newCaracter = await getChar(character);
-    console.log(newCaracter);
-  }
-};
+if (!movieId) {
+  console.log('Usage: node 101-starwars_characters.js <Movie ID>');
+} else {
+  const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-getAll(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`);
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else if (response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+
+      // Obtener la lista de personajes en el orden correcto
+      const characterUrls = movieData.characters;
+
+      // Función para obtener y mostrar los nombres de los personajes
+      function getCharacterName(index) {
+        if (index < characterUrls.length) {
+          request.get(characterUrls[index], (charError, charResponse, charBody) => {
+            if (charError) {
+              console.error(charError);
+            } else if (charResponse.statusCode === 200) {
+              const characterData = JSON.parse(charBody);
+              console.log(characterData.name);
+              getCharacterName(index + 1); // Llamar recursivamente para el siguiente personaje
+            } else {
+              console.error(`Request failed with status code: ${charResponse.statusCode}`);
+            }
+          });
+        }
+      }
+
+      // Comenzar la secuencia para obtener los nombres de los personajes
+      getCharacterName(0);
+    } else {
+      console.error(`Request failed with status code: ${response.statusCode}`);
+    }
+  });
+}
