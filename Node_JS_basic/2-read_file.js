3eacc1a6d79c2f1447257cd2cfe6b9ba6fd362b4
Alexander1455 <alexander21111121@gmail.com>
2023-11-02 21:11:48 -0500

    read file syncrona

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,41 +1,40 @@
 const fs = require('fs');
 
 function countStudents(path) {
+  let content;
+
   try {
-    // Lee el contenido del archivo CSV de manera síncrona
-    const data = fs.readFileSync(path, 'utf8');
-
-    // Divide el contenido del archivo en líneas
-    const lines = data.split('\n');
-
-    // Inicializa objetos para contar estudiantes por campo
-    const studentCount = {
-      CS: 0,
-      SWE: 0,
-    };
-    const studentList = {
-      CS: [],
-      SWE: [],
-    };
-
-    // Itera a través de las líneas para contar estudiantes y agregar nombres a las listas
-    for (const line of lines) {
-      const [firstname, , , field] = line.split(',');
-      if (field === 'CS') {
-        studentCount.CS += 1;
-        studentList.CS.push(firstname);
-      } else if (field === 'SWE') {
-        studentCount.SWE += 1;
-        studentList.SWE.push(firstname);
-      }
-    }
-
-    // Muestra el resultado en la consola
-    console.log('Number of students:', studentCount.CS + studentCount.SWE);
-    console.log(`Number of students in CS: ${studentCount.CS}. List: ${studentList.CS.join(', ')}`);
-    console.log(`Number of students in SWE: ${studentCount.SWE}. List: ${studentList.SWE.join(', ')}`);
-  } catch (error) {
+    content = fs.readFileSync(path);
+  } catch (err) {
     throw new Error('Cannot load the database');
+  }
+
+  content = content.toString().split('\n');
+
+  let students = content.filter((item) => item);
+
+  students = students.map((item) => item.split(','));
+
+  const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
+  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+
+  const fields = {};
+  for (const i in students) {
+    if (i !== 0) {
+      if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+      fields[students[i][3]].push(students[i][0]);
+    }
+  }
+
+  delete fields.field;
+
+  for (const key of Object.keys(fields)) {
+    console.log(
+      `Number of students in ${key}: ${fields[key].length}. List: ${fields[
+        key
+      ].join(', ')}`,
+    );
   }
 }
 
